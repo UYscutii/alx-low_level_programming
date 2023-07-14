c58d69246ebb1d2345516ab4731e13e240749db9
UYscutii <walkerkarl361@gmail.com>
2023-07-14 11:55:16 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description: a c program that prints using puts function
  * Return : always returns 0
  */
-main(void){
+int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
