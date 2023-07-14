a65f238fbc0554b5c5a4b8f728bd2e9fc0ad85c0
UYscutii <walkerkarl361@gmail.com>
2023-07-14 11:30:47 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-main () {
+#include <stdio.h>
+/**
+ * main: main is the entry point
+ * Description: a code using puts function to print a sentence
+ * Return : always returns 0
+ */
+main(void){
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
+return (0);
 }
