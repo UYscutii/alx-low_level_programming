367a1b9454cda5920b4087d8156f06cbe504e8e0
UYscutii <walkerkarl361@gmail.com>
2023-07-14 11:51:22 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main: this is the main function it is the entry point of the program i wrote its so useful
- * Description: a code using puts function to print a sentence
+ * main :Main - Entry point
+ * Description: a c program that prints using puts function
  * Return : always returns 0
  */
 main(void){
