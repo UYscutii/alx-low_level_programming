2719c7207215c62d5d388c740fae85a0a7bb572c
UYscutii <walkerkarl361@gmail.com>
2023-07-15 10:24:26 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * Description: printing a sentence using printf
+ * Return : always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
