2bbfb309f2fcb364bf559ecf64c34e0bbed56573
UYscutii <walkerkarl361@gmail.com>
2023-07-18 18:30:04 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - entry point
+*description: printing alphabets in lowercase
+*Return : always 0
+ */
+void print_alphabet(void)
+{
+	int x;
+	int y;
+
+	for (y = 1; y  <= 10; y++)
+	{
+	for (x = 'a'; x  <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	}
+	_putchar('\n');
+	}
