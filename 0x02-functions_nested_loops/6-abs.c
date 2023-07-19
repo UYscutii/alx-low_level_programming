1a20837f5316739f214b2b5dcb8ab11b5cd056cc
UYscutii <walkerkarl361@gmail.com>
2023-07-18 19:37:17 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * main entry point
+ * return: always 0(success)
+ **/
+int _abs(int n)
+	{
+	if (n > 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (-n);
+	}
+	_putchar('\n');
+	return (0);
+	}
+	
