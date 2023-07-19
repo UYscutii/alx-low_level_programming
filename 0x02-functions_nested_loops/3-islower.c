f0c6dc8ac7aec83f48fdb6873aad1392ac3a684c
UYscutii <walkerkarl361@gmail.com>
2023-07-18 18:48:44 -0700

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - entry point
+*Description: returning 0 if loercase and 1 if uppercase
+Return : 1 or 0
+*/
+int _islower(int c)
+	{
+	if (c >= 97 && c >= 122)
+	{
+	return (0);
+	}
+	else
+	{
+	return (1);
+	}
+	_putchar('\n');
