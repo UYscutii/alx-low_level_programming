c54b21edba763e6847dded603b14b2d0215f68c8
UYscutii <walkerkarl361@gmail.com>
2023-07-18 19:22:22 -0700

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * main - entry point
+ * desription; writing a function that returns different numbers
+ * Retunr: return 0 1 or -1
+ **/
+int print_sign(int n)
+	{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	if (n == 0)
+	{
+	_putchar(0);
+	return (0);
+	}
+	if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	_putchar('\n');
+	}
