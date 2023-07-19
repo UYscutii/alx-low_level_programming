ebf2fae08f73600441bdff9ecae37279fe962e20
UYscutii <walkerkarl361@gmail.com>
2023-07-18 19:08:31 -0700

    done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - entry point
+*Description: returning 0 if loercase and 1 if uppercase
+Return : 1 or 0
+*/
+int _isalpha(int c)
+	{
+	if (c >= 65 && c <= 90 || c >= 97  && c <= 122)
+	{
+	return (1);
+	};
+	else
+	{
+	return (0);
+	}
+	_puthar('\n');
+	}
