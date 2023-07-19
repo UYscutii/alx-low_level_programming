9e451333811bc7d4d7dd9e232053664174690d3f
UYscutii <walkerkarl361@gmail.com>
2023-07-18 18:33:26 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,14 @@
 *description: printing alphabets in lowercase
 *Return : always 0
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int x;
 	int y;
 
 	for (y = 1; y  <= 10; y++)
 	{
+	
 	for (x = 'a'; x  <= 'z'; x++)
 	{
 	_putchar(x);
