ab05affa6adadc65538d53c7c8f34fd9e91ccb68
UYscutii <walkerkarl361@gmail.com>
2023-07-20 10:59:37 -0700

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#incude "main.h"
+/*
+ *main - entry point
+ *Description: printing most numbers
+ *Return : always 0 (success)
+ **/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+{
+	if (n == 50 || n == 52)
+		continue;
+		_purchar('n');
+}
+	_putchar('\n');
+}
