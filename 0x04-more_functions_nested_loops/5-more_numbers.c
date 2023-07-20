bd1b25b3e742b4ee4479ca27ab2309a5466a5887
UYscutii <walkerkarl361@gmail.com>
2023-07-20 11:34:48 -0700

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ *main - entry point
+ *Description: printing most numbers
+ *Return : always 0 (success)
+ **/
+void more_numbers(void)
+{
+	int n 
+	
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ *main - entry point
+ *Description: printing most numbers
+ *Return : always 0 (success)
+ **/
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+{
+	for (x = 1; x <= n; x++)
+		_putchar('_');
+		_putchar('\n');
+}
+}
