6e0db7ff8916250233fb6a7166a038c155da5a9b
UYscutii <walkerkarl361@gmail.com>
2023-07-20 15:22:13 -0700

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+/*
+ *main - entry point
+ *Description: printing most numbers
+ *Return : always 0 (success)
+ **/
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{
+		if ( n % 3 == 0 && !(n % 5 == 0))
+		       printf("Fizz");
+		else if ( n % 5 == 0 && !(n % 3 == 0))
+			printf("Buzz");
+		else if ( n % 5 == 0 &&  n % 3 == 0)
+			printf("FizzBuzz");
+		else 
+		{
+			printf("%d",n);
+		}
+			printf(" ");
+	}
+			return (0);
+}	
+
