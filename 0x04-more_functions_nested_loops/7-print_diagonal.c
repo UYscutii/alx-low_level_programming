f7d23c03657e9fd70f46d28affcade830c4c3280
UYscutii <walkerkarl361@gmail.com>
2023-07-20 12:19:52 -0700

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,22 +5,22 @@
  *Return : always 0 (success)
  **/
 void print_diagonal(int n)
-{       
-        int space;
-        int slash;
-        
-        if (n <= 0)
-                _putchar('\n');
-        else
-        {
-                for (slash = 1; slash <= n; slash++)
+{
+	int space;
+	int slash;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (slash = 1; slash <= n; slash++)
+		{
+					for (space = 1; space < n ; space++)
 			{
-				_putchar(47);
-					for (space = 0; space < n ; space ++)
-				{
 					_putchar(32);
+					_putchar(47);
 					_putchar('\n');
-				}
 			}
+		}
 	}
 }
