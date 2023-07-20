ae7bc76914d62dfce58cb93ed108ec3e1629abe1
UYscutii <walkerkarl361@gmail.com>
2023-07-20 09:55:08 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/*
+ * main - entry point
+ * Description: checking if a character is upper or lower case
+ * Return : 0 lowercase if 1 if uppercase
+ **/
+int _isupper(int c)
+	{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+	return (0);
+	}
+	}
