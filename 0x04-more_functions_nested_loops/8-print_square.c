c26af136f7aff099b27aadc5019d26262e65622c
UYscutii <walkerkarl361@gmail.com>
2023-07-20 14:39:47 -0700

    done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ *main - entry point
+ *Description: printing most numbers
+ *Return : always 0 (success)
+ **/
+void print_square(int size)
+{
+	int c, r;
+
+	for (r = 1; r <= size; r++)
+		{
+			for (c = 1; c <= size; c++)
+				{ 
+					_putchar(35);
+			        }
+			_putchar('\n');
+		}
+			        
