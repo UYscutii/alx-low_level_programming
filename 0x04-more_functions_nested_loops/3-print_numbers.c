4a98568ebd005316eddb386f6fa73bd30067f57a
UYscutii <walkerkarl361@gmail.com>
2023-07-20 10:31:54 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ *main - entry point
+ *Description : printing numbers from 0 to 9
+ *Return : always 0 (success)
+**/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+{
+		_putchar('n');
+}
+	_putchar('\n');
+}	
