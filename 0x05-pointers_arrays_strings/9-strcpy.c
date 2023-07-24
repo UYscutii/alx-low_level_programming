0a3cdbf181c94622759fa6b83b63e6c8932ca8c4
UYscutii <walkerkarl361@gmail.com>
2023-07-24 12:21:59 -0700

    done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	do 
+	{
+		i++
+		src[i] = dest[i];
+	}
+	while (src[i] != '\0');
+	return (dest);
+}
