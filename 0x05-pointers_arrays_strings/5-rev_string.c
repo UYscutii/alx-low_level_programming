a59aa8e2b6fea4d6a268589610a3bba2841691a0
UYscutii <walkerkarl361@gmail.com>
2023-07-24 14:39:05 -0700

    done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int x,y;
+	char temp;
+
+	while (s[x] != '\0')
+		x++;
+	for (y = 0; y < x / 2; y++)
+	{
+		temp = s[y];
+		s[y] = s[x - 1 - y];
+		s[x - 1 - y] = temp;
+	}
+}
