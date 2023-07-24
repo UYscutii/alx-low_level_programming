40ae779650ce33b3c4aae8d240c67af9176429f9
UYscutii <walkerkarl361@gmail.com>
2023-07-24 11:52:59 -0700

    done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i <= n)
+		printf("%d ,",a[i]);
+		else if (i == n)
+		printf("%d",a[n]);
+	}
+	printf("\n");
+}
