cf0375b0942f1717173871586b4ee5945800a7c8
UYscutii <walkerkarl361@gmail.com>
2023-07-27 09:35:41 -0700

    done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	while (src[i] != '\0')
+	{
+		dest[len + i + 1] = src[i];
+		i++;
+	}
+		dest[len + i + 1] = '\0';
+		return (dest);
+}
