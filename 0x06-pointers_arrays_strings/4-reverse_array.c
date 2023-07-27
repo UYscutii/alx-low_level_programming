92dbea1457628a3c121752bcc729c4d490fa4e8d
UYscutii <walkerkarl361@gmail.com>
2023-07-27 15:05:25 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-reverse_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0, j = n; i < n; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
