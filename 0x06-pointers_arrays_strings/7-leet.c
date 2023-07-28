dd56ee69a8d624cffe2c2b486f9ea510b7239127
UYscutii <walkerkarl361@gmail.com>
2023-07-27 17:21:05 -0700

    done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+char *leet(char *c)
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433006611";
+	
+	for (i = 0; c[i] != '\0'; i++)
+		{
+			for (j = 0; j < 10; j++)
+				{
+					if (n[i] == s1[j])
+					{
+						n[i] = s2[j];
+					}
+					
+				}			
+		}
+	return (n);
