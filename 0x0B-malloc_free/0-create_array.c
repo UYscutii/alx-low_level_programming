80428e3775ace2ff2de73da087981bce8e58d6ff
UYscutii <walkerkarl361@gmail.com>
2023-08-08 09:29:44 -0700

    done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+	if (size == 0 || n = 0)
+		return (0);
+	while (size--);
+	n[size] = c;
+		return (n);
+}
