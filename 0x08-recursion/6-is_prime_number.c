7105e5da4360b7463e152ac8d8a25dd8d539511c
UYscutii <walkerkarl361@gmail.com>
2023-08-30 19:53:30 -0700

    done

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+int check_prime(int n, int othrn)
+{
+	if (othrn >= n && n > 1)
+		return (1);
+	else if ( othrn % n == 0 || n <= 1)
+		return (0);
+	else 
+		return (check_prime(n, othrn + 1));
+}
