c070136aa0d0c92f45272a785031d8d5ba7d4d93
UYscutii <walkerkarl361@gmail.com>
2023-09-05 10:19:07 -0700

    done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	int x = 0;
+	char *i;
+
+	if ( str == NULL)
+		return (NULL);
+	while ( str[size] != '\0')
+		size++;
+	i = malloc(size * sizeof(*str + 1));
+	if (i == 0) 
+		return (NULL);
+	else 
+	{
+		for (; x < size; x++)
+			i[x] = str[x];
+	}
+	return (i);
+}
