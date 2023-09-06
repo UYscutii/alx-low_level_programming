34a4ee7e87693631f1816cc91b505f9ea3f3fba0
UYscutii <walkerkarl361@gmail.com>
2023-09-05 17:30:22 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,12 +24,12 @@ int **alloc_grid(int width, int height)
 		for (i = 0; i < height; i++)
 			tab[i] = malloc((sizeof(**tab) * width);
 			if (tab[i] == 0)
-		{
+	{
 		while (i--)
-		free((tab[i]);
+		free(tab[i]);
 		free (tab);
 		return (NULL);
-		}
+	}
 		 for (j = 0; j < width; j++)
 		 tab[i][j] = 0;
 	}
