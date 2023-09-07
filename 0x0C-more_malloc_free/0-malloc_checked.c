41aab697687f4522994ad975e2d7344ed31fd267
UYscutii <walkerkarl361@gmail.com>
2023-09-07 09:02:12 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *x
+
+	x = malloc(b);
+	if (x == 0)
+		exit(98);
+	return (x);
+}
