b9a5647c21408d6880c07509bc950aab97f5a55e
UYscutii <walkerkarl361@gmail.com>
2023-09-07 18:46:41 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,7 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 		h = malloc(nmemb * size);
-		if (h == NULL)
+		if (h == 0)
 		return (NULL);
 		_memset(h, 0, nmemb * size);
 		return (h);
