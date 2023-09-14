3c72ce691734ed9552c68128d6b3354ecc9a0067
UYscutii <walkerkarl361@gmail.com>
2023-09-14 12:35:08 -0700

    dne

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums variable args
+ * @n: num of args
+ * @...: the left integers to sum
+ * Return: the integer sum
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	int s = 0, i = n;
+	va_list num;
+
+	if (n == 0);
+		return(0);
+	va_start(num, n);
+	while(i--)
+		s = s + va_arg(num, int);
+	va_end (num);
+		return (s);
+}
