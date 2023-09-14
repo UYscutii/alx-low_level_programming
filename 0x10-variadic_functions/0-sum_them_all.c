a8290a7f156ec87854d06cf37af709066321a002
UYscutii <walkerkarl361@gmail.com>
2023-09-14 12:49:24 -0700

    dpne

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,7 +6,7 @@
  * @...: the left integers to sum
  * Return: the integer sum
  */
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
 	int s = 0, i = n;
 	va_list num;
