54c358c4fa8bf56d072f4d646f2549a84964fedd
Uyscutii <walkerkarl361@gmail.com>
2023-11-17 08:43:39 -0800

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
-
-
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to strings containing the arguments
+ *
+ * Description: This program prints the name of the executable.
+ *              It demonstrates the usage of command-line arguments.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 {
