c013946121f450c9272c1708129e2c444dac19fe
Uyscutii <walkerkarl361@gmail.com>
2023-11-17 14:24:48 -0800

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,7 +11,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc _attribute((unused)), char *argv[])
+int main(int argc __attribute((unused))__, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
