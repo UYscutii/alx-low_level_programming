3a9e89fa01c3e258ac0e76ccfac955735cd91b9d
Uyscutii <walkerkarl361@gmail.com>
2023-11-17 08:40:52 -0800

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,8 @@
 
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc
 	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+
+
+
+int main(int argc, char *argc[])
+{
+	printf("%d",argc - 1);
+	return (0);
+}
