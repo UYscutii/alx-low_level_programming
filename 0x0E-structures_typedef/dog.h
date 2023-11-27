9866aaa0f551e95e5e16de7ee07b884e2684079f
Uyscutii <walkerkarl361@gmail.com>
2023-11-27 10:23:49 -0800

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-
-
-
-
+/**
+ * struct dog - a struct dog
+ * @name : dog's name
+ * @owner: owner's name
+ * @age : dog's age
+ * description : a struct of dogs
+ */
 
 struct dog
 {
 	char *name;
-	char *owner
+
+	char *owner;
+
 	float age;
 }
 #endif
